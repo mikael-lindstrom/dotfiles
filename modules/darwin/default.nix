a6ef6b52b14babfc783e3b6efcaf88b1de1e9acb
Mikael Lindström <mikael.lindstrom@gmail.com>
2025-04-28 12:35:18 +0200

    Decrease icons in MacOS dock

--- modules/darwin/default.nix
+++ modules/darwin/default.nix
@@ -30,6 +30,7 @@
   system.defaults.NSGlobalDomain.InitialKeyRepeat = 15;
   system.defaults.NSGlobalDomain.KeyRepeat = 2;
   system.defaults.dock.autohide = true;
+  system.defaults.dock.tilesize = 36;
   system.defaults.controlcenter.Bluetooth = true;
   system.defaults.controlcenter.Sound = true;
 
